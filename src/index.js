96470e2fc0adf5218e2443e7ec9c3b4e5b090234
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-02-04 21:29:43 +0530

    create ship class
    1.Create ship class &
    2.Add ship constructor &
    3.Add takeHit function which raises the hit counter &
    4.Add isSunk function which checks if the number of hits is enough to sink ship.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-let a = 0;
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hit = 0;
+    this.sunk = false;
+  }
+
+  takeHit() {
+    this.hit += 1;
+  }
+
+  isSunk() {
+    if (this.hit === this.length) {
+      this.sunk = true;
+    }
+  }
+}
