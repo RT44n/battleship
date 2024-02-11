195a5af456738352b1481be95457892f8b6139a4
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-02-11 14:09:23 +0530

    Add player class

---
+++ src/player.js
@@ -0,0 +1,30 @@
+import Gameboard from "./gameboard";
+
+class Player {
+  constructor(name = "computer") {
+    this.playerName = name;
+    this.playerBoard = new Gameboard();
+  }
+
+  hit(x, y) {
+    this.playerBoard.takeHit(x, y);
+  }
+
+  randomHit() {
+    this.playerBoard.randomAttack();
+  }
+
+  hasLost() {
+    this.playerBoard.allSunk();
+  }
+
+  placeShip() {
+    this.playerBoard.placeShipRandom();
+  }
+
+  showBoard() {
+    this.playerBoard.checkBoard();
+  }
+}
+
+export default Player;
